56ba2c3a3b80da00853ffaac4c6bd46a2bdddab2
kail0n <kailontse@gmail.com>
2020-11-13 17:01:22 +0000

    add displayEntries function

--- client/client.js
+++ client/client.js
@@ -14,11 +14,20 @@ getAllJournalEntries();
 function getAllJournalEntries(){
     fetch('http://localhost:3000/journal')
     .then(r=>r.json())
-    .then(appendEntry)
+    .then(displayEntries)
     .catch(console.warn)
 };
 
 function submitEntry(e){
     e.preventDefault();
 
+};
+
+function displayEntries(entries){
+    const htmlString = entries
+        .map(entries => {
+            return `<p class="journalEntry">${entries}</p>`;
+        })
+        .join(' ');
+    
 };
