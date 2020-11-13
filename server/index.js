aea7b14a96ad35cdf58dc509a6d28819ac44934e
kail0n <kailontse@gmail.com>
2020-11-13 19:22:22 +0000

    clean code

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-const server = require('./server')
+const server = require('./server');
 const port = 3000;
 
-server.listen(port, () => console.log(`Express running from port ${port}`))
+server.listen(port, () => console.log(`Express running from port ${port}`));
