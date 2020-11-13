660598c27f4d141ccbec38d6dab3693793836c0f
kail0n <kailontse@gmail.com>
2020-11-13 19:23:57 +0000

    basic set up client.js file, need to connect to json server (incomplete)

--- client/client.js
+++ client/client.js
@@ -11,23 +11,23 @@ getAllJournalEntries();
 
 // ********************************************
 
-function getAllJournalEntries(){
-    fetch('http://localhost:3000/journal')
-    .then(r=>r.json())
-    .then(displayEntries)
-    .catch(console.warn)
-};
+// function getAllJournalEntries(){
+//     fetch('http://localhost:3000/journal')
+//     .then(r=>r.json())
+//     .then(displayEntries)
+//     .catch(console.warn)
+// };
 
-function submitEntry(e){
-    e.preventDefault();
+// function submitEntry(e){
+//     e.preventDefault();
 
-};
+// };
 
-function displayEntries(entries){
-    const htmlString = entries
-        .map(entries => {
-            return `<p class="journalEntry">${entries}</p>`;
-        })
-        .join(' ');
+// function displayEntries(entries){
+//     const htmlString = entries
+//         .map(entries => {
+//             return `<p class="journalEntry">${entries}</p>`;
+//         })
+//         .join(' ');
     
-};
+// };
