59550a33297a2de1ca43539509f5751ddfa3ee09
Geraldine Bengsch <geraldine_be@hotmail.com>
2020-11-17 10:09:42 +0000

    emoji selector (work in progress) generalise function

--- client/interactive.js
+++ client/interactive.js
@@ -1,17 +1,29 @@
-function selectEmoji() {
-    document.getElementById("emojiInd").classList.toggle("show");
-  }
-  
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.chooseDrop')) {
-      const dropdowns = document.getElementsByClassName("emojiContent");
-      const i;
-      for (i = 0; i < dropdowns.length; i++) {
-        let openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
+const btn = document.querySelector('.emoji-button');
+const container = document.getElementById('emojiContainer');
+
+btn.addEventListener('click', (e) => {
+    createEmoji();
+    //experimental feature of how many "likes"
+    //btn.innerHTML = `Click count: ${event.detail}`;
+
+});
+// on click of one of the elements, selects the element and pastes it into a new div container
+// needs to be createElement, because it adds the content to the nav bar otherwise
+// innerHTML allows display of emoji, unlike textContent
+// work in progress: creating a loop that checks if there is an emoji already chosen
+function createEmoji() {
+    let emoji = document.querySelector('emoji')
+    if (!emoji) {
+        console.log("emoji not chosen")
+    const pasteEmoji = document.createElement
+    ('emoji');
+    console.log(pasteEmoji)
+    let reply = () => {
+        pasteEmoji.srcElement.innerHTML = reply;
     }
-  }
+    
+        container.appendChild(pasteEmoji);
+    console.log(container) } else {
+       alert("You already chose your mood")
+    } 
+}
